25462ad07cf7ac2698c6cedad9030876dbc2a6e9
Ahmad Muhammad <theahmadm3@gmail.com>
2024-04-16 20:14:33 +0100

    Added Side bars

---
+++ src/components/SideBar/PhoneSideBar.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Offcanvas from "react-bootstrap/Offcanvas";
+
+const PhoneSideBar: React.FC = () => {
+	const [show, setShow] = useState<boolean>(false);
+
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+
+	const userType = "student"; // TODO: get this dynamically
+
+	return (
+		<>
+			<Button variant="primary" onClick={handleShow}>
+				Launch
+			</Button>
+
+			{userType === "student" ? (
+				<Offcanvas show={show} onHide={handleClose}>
+					<Offcanvas.Header closeButton></Offcanvas.Header>
+					<Offcanvas.Body>
+						<p>Profile</p>
+						<p>Courses</p>
+						<p>Attendance</p>
+						<p>Settings</p>
+						<p>Logout</p>
+					</Offcanvas.Body>
+				</Offcanvas>
+			) : (
+				<Offcanvas show={show} onHide={handleClose}>
+					<Offcanvas.Header closeButton></Offcanvas.Header>
+					<Offcanvas.Body>
+						<p>Profile</p>
+						<p>Courses</p>
+						<p>Attendance</p>
+						<p>Attendance Records</p>
+						<p>Settings</p>
+						<p>Logout</p>
+					</Offcanvas.Body>
+				</Offcanvas>
+			)}
+		</>
+	);
+};
+export default PhoneSideBar;
---
+++ src/components/SideBar/SideBar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+// import { NavLink } from "react-router-dom";
+
+const SideBar: React.FC = () => {
+	return (
+		<></>
+	);
+};
+export default SideBar;
