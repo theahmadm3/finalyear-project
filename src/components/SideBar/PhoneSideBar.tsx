fdd277789e6f7156ba7ffe6ce5aeb2f6df4f58ab
Ahmad Muhammad <theahmadm3@gmail.com>
2024-04-21 01:59:50 +0100

    edited sidebar

--- src/components/SideBar/PhoneSideBar.tsx
+++ src/components/SideBar/PhoneSideBar.tsx
@@ -10,7 +10,7 @@ const PhoneSideBar: React.FC = () => {
 	const handleClose = () => setShow(false);
 	const handleShow = () => setShow(true);
 
-	const { logout, isStudent } = useContext(AuthContext);
+	const { logout } = useContext(AuthContext);
 
 	return (
 		<>
@@ -18,119 +18,67 @@ const PhoneSideBar: React.FC = () => {
 				<i className="material-icons white">menu</i>
 			</button>
 
-			{isStudent ? (
-				<Offcanvas show={show} onHide={handleClose} className="w-50">
-					<Offcanvas.Header
-						className="bg-dark-blue white"
-						closeButton
-					></Offcanvas.Header>
-					<Offcanvas.Body className="bg-dark-blue white">
-						<Link
-							onClick={handleClose}
-							to="/portal"
-							className="inline-flex w-100 link white hover-bg-blue pa1"
-							style={{ outline: "none" }}
-						>
-							<i className="material-icons mr2">account_circle</i>{" "}
-							<p className="ma0">Profile</p>
-						</Link>
-						<Link
-							onClick={handleClose}
-							to="/portal"
-							className="inline-flex w-100 link white hover-bg-blue pa1"
-							style={{ outline: "none" }}
-						>
-							<i className="material-icons mr2">school</i>{" "}
-							<p className="ma0">Courses</p>
-						</Link>
-						<Link
-							onClick={handleClose}
-							to="/portal"
-							className="inline-flex w-100 link white hover-bg-blue pa1"
-							style={{ outline: "none" }}
-						>
-							<i className="material-icons mr2">check_box</i>{" "}
-							<p className="ma0">Attendance</p>
-						</Link>
-						<Link
-							onClick={handleClose}
-							to="/portal"
-							className="inline-flex w-100 link white hover-bg-blue pa1"
-							style={{ outline: "none" }}
-						>
-							<i className="material-icons mr2">featured_play_list</i>{" "}
-							<p className="ma0"> Attendance Records</p>
-						</Link>
-						<Link
-							onClick={handleClose}
-							to="/portal"
-							className="inline-flex w-100 link white hover-bg-blue pa1"
-							style={{ outline: "none" }}
-						>
-							<i className="material-icons mr2">settings</i>{" "}
-							<p className="ma0">Settings</p>
-						</Link>
-						<div
-							onClick={logout}
-							className="inline-flex w-100 link white hover-bg-blue pa1"
-							style={{ outline: "none" }}
-						>
-							<i className="material-icons mr2">logout</i>{" "}
-							<p className="ma0">Logout</p>
-						</div>
-					</Offcanvas.Body>
-				</Offcanvas>
-			) : (
-				<Offcanvas show={show} onHide={handleClose}>
-					<Offcanvas.Header closeButton></Offcanvas.Header>
-					<Offcanvas.Body>
-						<Link
-							onClick={handleClose}
-							to="/portal"
-							className="inline-flex w-100 link white hover-bg-blue pa1"
-							style={{ outline: "none" }}
-						>
-							<i className="material-icons mr2">account_circle</i>{" "}
-							<p className="ma0">Profile</p>
-						</Link>
-						<Link
-							onClick={handleClose}
-							to="/portal"
-							className="inline-flex w-100 link white hover-bg-blue pa1"
-							style={{ outline: "none" }}
-						>
-							<i className="material-icons mr2">school</i>{" "}
-							<p className="ma0">Courses</p>
-						</Link>
-						<Link
-							onClick={handleClose}
-							to="/portal"
-							className="inline-flex w-100 link white hover-bg-blue pa1"
-							style={{ outline: "none" }}
-						>
-							<i className="material-icons mr2">check_box</i>{" "}
-							<p className="ma0">Attendance</p>
-						</Link>
-						<Link
-							onClick={handleClose}
-							to="/portal"
-							className="inline-flex w-100 link white hover-bg-blue pa1"
-							style={{ outline: "none" }}
-						>
-							<i className="material-icons mr2">settings</i>{" "}
-							<p className="ma0">Settings</p>
-						</Link>
-						<div
-							onClick={logout}
-							className="inline-flex w-100 link white hover-bg-blue pa1"
-							style={{ outline: "none" }}
-						>
-							<i className="material-icons mr2">logout</i>{" "}
-							<p className="ma0">Logout</p>
-						</div>
-					</Offcanvas.Body>
-				</Offcanvas>
-			)}
+			<Offcanvas show={show} onHide={handleClose} className="w-50">
+				<Offcanvas.Header
+					className="bg-dark-blue white"
+					closeButton
+				></Offcanvas.Header>
+				<Offcanvas.Body className="bg-dark-blue white">
+					<Link
+						onClick={handleClose}
+						to="/portal"
+						className="inline-flex w-100 link white hover-bg-blue pa1"
+						style={{ outline: "none" }}
+					>
+						<i className="material-icons mr2">account_circle</i>{" "}
+						<p className="ma0">Profile</p>
+					</Link>
+					<Link
+						onClick={handleClose}
+						to="/courses"
+						className="inline-flex w-100 link white hover-bg-blue pa1"
+						style={{ outline: "none" }}
+					>
+						<i className="material-icons mr2">school</i>{" "}
+						<p className="ma0">Courses</p>
+					</Link>
+					<Link
+						onClick={handleClose}
+						to="/attendance"
+						className="inline-flex w-100 link white hover-bg-blue pa1"
+						style={{ outline: "none" }}
+					>
+						<i className="material-icons mr2">check_box</i>{" "}
+						<p className="ma0">Attendance</p>
+					</Link>
+					<Link
+						onClick={handleClose}
+						to="/records"
+						className="inline-flex w-100 link white hover-bg-blue pa1"
+						style={{ outline: "none" }}
+					>
+						<i className="material-icons mr2">featured_play_list</i>{" "}
+						<p className="ma0"> Attendance Records</p>
+					</Link>
+					<Link
+						onClick={handleClose}
+						to="/settings"
+						className="inline-flex w-100 link white hover-bg-blue pa1"
+						style={{ outline: "none" }}
+					>
+						<i className="material-icons mr2">settings</i>{" "}
+						<p className="ma0">Settings</p>
+					</Link>
+					<div
+						onClick={logout}
+						className="inline-flex w-100 link white hover-bg-blue pa1 pointer"
+						style={{ outline: "none" }}
+					>
+						<i className="material-icons mr2">logout</i>{" "}
+						<p className="ma0">Logout</p>
+					</div>
+				</Offcanvas.Body>
+			</Offcanvas>
 		</>
 	);
 };
--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -7,133 +7,74 @@ const SideBar: React.FC = () => {
 		outline: "none",
 	};
 
-	const { logout, isStudent } = useContext(AuthContext);
+	const { logout } = useContext(AuthContext);
 
 	return (
 		<div className="flex flex-column vh-100">
-			{isStudent ? (
-				<>
-					<NavLink
-						to="/portal"
-						style={linkStyles}
-						className={({ isActive }) =>
-							isActive
-								? "pa2 inline-flex link white bg-white blue items-center"
-								: "pa2 inline-flex link white items-center"
-						}
-					>
-						<i className="material-icons mr2">account_circle</i>
-						<p className="ma0">Profile</p>
-					</NavLink>
-					<NavLink
-						to="/courses"
-						style={linkStyles}
-						className={({ isActive }) =>
-							isActive
-								? "pa2 inline-flex link white bg-white blue items-center"
-								: "pa2 inline-flex link white items-center"
-						}
-					>
-						<i className="material-icons mr2">school</i>
-						<p className="ma0">Courses</p>
-					</NavLink>
-					<NavLink
-						to="/attendance"
-						style={linkStyles}
-						className={({ isActive }) =>
-							isActive
-								? "pa2 inline-flex link white bg-white blue items-center"
-								: "pa2 inline-flex link white items-center"
-						}
-					>
-						<i className="material-icons mr2">check_box</i>
-						<p className="ma0">Attendance</p>
-					</NavLink>
-					<NavLink
-						to="/records"
-						style={linkStyles}
-						className={({ isActive }) =>
-							isActive
-								? "pa2 inline-flex link white bg-white blue items-center"
-								: "pa2 inline-flex link white items-center"
-						}
-					>
-						<i className="material-icons mr2">featured_play_list</i>
-						<p className="ma0">Attendance Records</p>
-					</NavLink>
-					<NavLink
-						to="/settings"
-						style={linkStyles}
-						className={({ isActive }) =>
-							isActive
-								? "pa2 inline-flex link white bg-white blue items-center"
-								: "pa2 inline-flex link white items-center"
-						}
-					>
-						<i className="material-icons mr2">settings</i>
-						<p className="ma0">Settings</p>
-					</NavLink>
-					<div onClick={logout} className="inline-flex white pa2">
-						<i className="material-icons mr2">logout</i>
-						Logout
-					</div>
-				</>
-			) : (
-				<>
-					<NavLink
-						to="/portal"
-						style={linkStyles}
-						className={({ isActive }) =>
-							isActive
-								? "pa2 inline-flex link white bg-white blue items-center"
-								: "pa2 inline-flex link white items-center"
-						}
-					>
-						<i className="material-icons mr2">account_circle</i>
-						<p className="ma0">Profile</p>
-					</NavLink>
-					<NavLink
-						to="/"
-						style={linkStyles}
-						className={({ isActive }) =>
-							isActive
-								? "pa2 inline-flex link white bg-white blue items-center"
-								: "pa2 inline-flex link white items-center"
-						}
-					>
-						<i className="material-icons mr2">school</i>
-						<p className="ma0">Courses</p>
-					</NavLink>
-					<NavLink
-						to="/"
-						style={linkStyles}
-						className={({ isActive }) =>
-							isActive
-								? "pa2 inline-flex link white bg-white blue items-center"
-								: "pa2 inline-flex link white items-center"
-						}
-					>
-						<i className="material-icons mr2">check_box</i>
-						<p className="ma0">Attendance</p>
-					</NavLink>
-					<NavLink
-						to="/"
-						style={linkStyles}
-						className={({ isActive }) =>
-							isActive
-								? "pa2 inline-flex link white bg-white blue items-center"
-								: "pa2 inline-flex link white items-center"
-						}
-					>
-						<i className="material-icons mr2">settings</i>
-						<p className="ma0">Settings</p>
-					</NavLink>
-					<div onClick={logout} className="inline-flex white pa2">
-						<i className="material-icons mr2">logout</i>
-						Logout
-					</div>
-				</>
-			)}
+			<NavLink
+				to="/portal"
+				style={linkStyles}
+				className={({ isActive }) =>
+					isActive
+						? "pa2 inline-flex link white bg-white blue items-center"
+						: "pa2 inline-flex link white items-center"
+				}
+			>
+				<i className="material-icons mr2">account_circle</i>
+				<p className="ma0">Profile</p>
+			</NavLink>
+			<NavLink
+				to="/courses"
+				style={linkStyles}
+				className={({ isActive }) =>
+					isActive
+						? "pa2 inline-flex link white bg-white blue items-center"
+						: "pa2 inline-flex link white items-center"
+				}
+			>
+				<i className="material-icons mr2">school</i>
+				<p className="ma0">Courses</p>
+			</NavLink>
+			<NavLink
+				to="/attendance"
+				style={linkStyles}
+				className={({ isActive }) =>
+					isActive
+						? "pa2 inline-flex link white bg-white blue items-center"
+						: "pa2 inline-flex link white items-center"
+				}
+			>
+				<i className="material-icons mr2">check_box</i>
+				<p className="ma0">Attendance</p>
+			</NavLink>
+			<NavLink
+				to="/records"
+				style={linkStyles}
+				className={({ isActive }) =>
+					isActive
+						? "pa2 inline-flex link white bg-white blue items-center"
+						: "pa2 inline-flex link white items-center"
+				}
+			>
+				<i className="material-icons mr2">featured_play_list</i>
+				<p className="ma0">Attendance Records</p>
+			</NavLink>
+			<NavLink
+				to="/settings"
+				style={linkStyles}
+				className={({ isActive }) =>
+					isActive
+						? "pa2 inline-flex link white bg-white blue items-center"
+						: "pa2 inline-flex link white items-center"
+				}
+			>
+				<i className="material-icons mr2">settings</i>
+				<p className="ma0">Settings</p>
+			</NavLink>
+			<div onClick={logout} className="inline-flex white pa2 pointer">
+				<i className="material-icons mr2">logout</i>
+				Logout
+			</div>
 		</div>
 	);
 };
