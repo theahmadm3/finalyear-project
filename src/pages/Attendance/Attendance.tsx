5bb8240e6621d0aa6dd231b047742317dfff66a9
Ahmad Muhammad <theahmadm3@gmail.com>
2024-05-02 12:05:24 +0100

    test 1 for scan qr code

--- src/pages/Attendance/Attendance.tsx
+++ src/pages/Attendance/Attendance.tsx
@@ -1,10 +1,16 @@
+/* eslint-disable no-extra-boolean-cast */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useContext, useState, useEffect } from "react";
+
 import { AuthContext } from "../../contexts/auth/AuthContext";
+import { QrReader } from "@chaiwei/react-qr-reader";
+
 import Loader from "../../components/Loader/Loader";
 
 const Attendance: React.FC = () => {
 	const { user } = useContext(AuthContext);
-	const [waiting, setWaiting] = useState(true);
+	const [waiting, setWaiting] = useState<boolean>(true);
+	const [data, setData] = useState<string>("No result");
 
 	useEffect(() => {
 		// When isStudent context value becomes available, update waiting state
@@ -17,7 +23,23 @@ const Attendance: React.FC = () => {
 		return <Loader />;
 	}
 
-	return <>Attendance</>;
+	return (
+		<>
+			<QrReader
+				onResult={(result: { text: string }, error: any) => {
+					if (!!result) {
+						setData(result?.text);
+					}
+
+					if (!!error) {
+						console.info(error);
+					}
+				}}
+				style={{ width: "100%" }}
+			/>
+			<p>{data}</p>
+		</>
+	);
 };
 
 export default Attendance;
