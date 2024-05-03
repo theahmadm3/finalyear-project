d29a0e652539b930a9cd8b4bd44fc01bc8c83a97
Ahmad Muhammad <theahmadm3@gmail.com>
2024-05-03 14:21:00 +0100

    staging for local testing

--- src/pages/Attendance/Attendance.tsx
+++ src/pages/Attendance/Attendance.tsx
@@ -5,31 +5,16 @@ import React, { useContext, useState, useEffect } from "react";
 import { AuthContext } from "../../contexts/auth/AuthContext";
 
 import Loader from "../../components/Loader/Loader";
-import { QrReader, OnResultFunction } from "react-qr-reader";
 
 const Attendance: React.FC = () => {
 	const { user } = useContext(AuthContext);
 	const [waiting, setWaiting] = useState<boolean>(true);
-	// const [qrCodeData, setQrCodeData] = useState<string | null>(null);
 
 	useEffect(() => {
-		// When isStudent context value becomes available, update waiting state
 		if (user !== undefined && user !== null) {
 			setWaiting(false);
 		}
-	}, [user]); // Update waiting state when isStudent changes
-
-	const handleScan: OnResultFunction = (qrResult: any, error: any) => {
-		if (qrResult) {
-			alert(qrResult.getText());
-		} else {
-			console.error(error);
-		}
-	};
-
-	// const handleError = (error: any) => {
-	// 	console.error(error);
-	// };
+	}, [user]);
 
 	if (waiting) {
 		return <Loader />;
@@ -37,13 +22,8 @@ const Attendance: React.FC = () => {
 
 	return (
 		<>
-			<QrReader
-				className="w-50"
-				onResult={handleScan}
-				constraints={{ facingMode: "environment" }}
-				scanDelay={300}
-			/>
-			{/* <p>{qrCodeData}</p> */}
+			{JSON.stringify(user)}
+			<button>Take Attendance</button>
 		</>
 	);
 };
